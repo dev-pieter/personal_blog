8d5fa30637be7189467acf626a67eb905f5f79fb
pietabrood <pieter_nortje@outlook.com>
2021-08-15 19:17:26 +0200

    Index sizing

--- src/lib/blog/posts/home/index.jsx
+++ src/lib/blog/posts/home/index.jsx
@@ -23,12 +23,12 @@ export default function Blog() {
         return (
             <>
                 <Center maxWidth='100vw'>
-                    <Stack maxWidth='80vw' p='10' className='blog-body'>
+                    <Stack maxWidth='80vw' p='10'>
                         <Center className='bg-change' minH='30vh' borderRadius='xl' p='2'>
                             <Heading fontSize='2xl' textColor='white' textAlign='center'>Hello friends 👋. Welcome to my blog 🚀.</Heading>
                         </Center>
                         <br/>
-                        <Box lineHeight='20px' whiteSpace='break-spaces'><ReactMarkdown>{markdown.text}</ReactMarkdown></Box>
+                        <Box className='blog-body' lineHeight='20px' whiteSpace='break-spaces'><ReactMarkdown>{markdown.text}</ReactMarkdown></Box>
                         <Center>
                             <HStack p={'10'} spacing={'5'}>
                                 <a href='https://github.com/pietabrood' className='home-i'>
