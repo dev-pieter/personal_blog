06de004e8b8f5894849a65ca84fef5fe06fe92db
dev-pieter <pieter.nortje@outlook.com>
2022-05-20 13:18:24 +0200

    fix: SEO optimization

--- shared/components/Seo/index.tsx
+++ shared/components/Seo/index.tsx
@@ -1,75 +1,75 @@
+import Head from "next/head";
 import React, { FunctionComponent } from "react";
-import { Helmet } from "react-helmet";
 
 interface Props {
   description?: string;
   lang?: string;
-  meta?: [{ name: string; content: string }];
+  meta?: Meta[];
   title: string;
   image?: string;
 }
 
-const SEO: FunctionComponent<Props> = ({
-  description,
-  lang,
-  meta,
-  title,
-  image,
-}) => {
+interface Meta {
+  name?: string;
+  content?: string;
+  property?: string;
+}
+
+const SEO: FunctionComponent<Props> = ({ description, lang, title, image }) => {
   const metaDescription =
     description ||
     "Welcome to my blog. Browse around, and if you like what you see, get in touch :)";
 
+  const metaTags: Meta[] = [
+    {
+      name: `description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      property: `og:image`,
+      content: image,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:image`,
+      content: image,
+    },
+    {
+      name: `twitter:creator`,
+      content: `Pieter Nortje`,
+    },
+    {
+      name: `twitter:title`,
+      content: title,
+    },
+    {
+      name: `twitter:description`,
+      content: metaDescription,
+    },
+  ];
+
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | Developer Blog`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:image`,
-          content: image,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:image`,
-          content: image,
-        },
-        {
-          name: `twitter:creator`,
-          content: `Pieter Nortje`,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta ?? [])}
-    />
+    <Head>
+      <title>{title} | dev-pieter</title>
+      {metaTags.map((tag) => (
+        <meta key={tag.name ?? tag.property} {...tag} />
+      ))}
+    </Head>
   );
 };
 
