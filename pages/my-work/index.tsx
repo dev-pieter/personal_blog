a7815a68fab61a6c1f75e235b091ed12b3e7ac5e
dev-pieter <pieter.nortje@outlook.com>
2022-05-23 12:48:03 +0200

    fix: Updated my-work page with SEO

--- pages/my-work/index.tsx
+++ pages/my-work/index.tsx
@@ -14,6 +14,7 @@ import React, { useEffect, useState } from "react";
 import { FaGithub, FaGithubAlt } from "react-icons/fa";
 import ReactMarkdown from "react-markdown";
 import PostBody from "../../shared/components/BlogComponents/PostBody";
+import SEO from "../../shared/components/Seo";
 import { ghApi, GHRepos, GHUser } from "../../shared/controllers/gitController";
 
 interface Props {
@@ -28,42 +29,49 @@ const Index: NextPage<Props> = ({ user, repos, readme }) => {
   }
 
   return (
-    <Stack border={"1px solid white"} padding="20px" gap={"20px"}>
-      <HStack gap="25px">
-        <Avatar
-          name={user.login}
-          src={user.avatar_url}
-          size="xl"
-          boxShadow={"0 0 5px black"}
-        ></Avatar>
-        <Stack>
-          <Heading fontFamily={"monospace"}>{user.login}</Heading>
-          <Text color={"gray.400"}>{user.location}</Text>
+    <>
+      <SEO title="My Work" image={user.avatar_url}></SEO>
+      <Stack border={"1px solid white"} padding="20px" gap={"20px"}>
+        <HStack gap="25px">
+          <Avatar
+            name={user.login}
+            src={user.avatar_url}
+            size="xl"
+            boxShadow={"0 0 5px black"}
+          ></Avatar>
+          <Stack>
+            <Heading fontFamily={"monospace"}>{user.login}</Heading>
+            <Text color={"gray.400"}>{user.location}</Text>
+          </Stack>
+          <Spacer />
+        </HStack>
+        <hr></hr>
+        <PostBody content={readme} />
+        <Stack gap="10px">
+          <Heading fontSize={"xl"}>GitHub Repos</Heading>
+          <SimpleGrid minChildWidth={{ base: "100%", md: "40%" }} gap="5px">
+            {repos.map((repo) => (
+              <HStack
+                key={repo.id}
+                border={"0.5px solid grey"}
+                borderRadius="4px"
+              >
+                <Stack padding="10px">
+                  <Link
+                    href={repo.html_url}
+                    target="_blank"
+                    fontWeight={"bold"}
+                  >
+                    {repo.full_name}
+                  </Link>
+                  <Text color={"gray.400"}>{repo.description}</Text>
+                </Stack>
+              </HStack>
+            ))}
+          </SimpleGrid>
         </Stack>
-        <Spacer />
-      </HStack>
-      <hr></hr>
-      <PostBody content={readme} />
-      <Stack gap="10px">
-        <Heading fontSize={"xl"}>GitHub Repos</Heading>
-        <SimpleGrid minChildWidth={{ base: "100%", md: "40%" }} gap="5px">
-          {repos.map((repo) => (
-            <HStack
-              key={repo.id}
-              border={"0.5px solid grey"}
-              borderRadius="4px"
-            >
-              <Stack padding="10px">
-                <Link href={repo.html_url} target="_blank" fontWeight={"bold"}>
-                  {repo.full_name}
-                </Link>
-                <Text color={"gray.400"}>{repo.description}</Text>
-              </Stack>
-            </HStack>
-          ))}
-        </SimpleGrid>
       </Stack>
-    </Stack>
+    </>
   );
 };
 
